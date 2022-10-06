c8cdf5d2561b768946a3d7f763999f43d78cf0ea
neomirage <neomirage@naver.com>
2022-10-06 15:20:32 +0900

    클래스의 인수를 public or backing field 테스트

--- src/test/kotlin/com/group/libraryapp/calculator/CalculatorTest.kt
+++ src/test/kotlin/com/group/libraryapp/calculator/CalculatorTest.kt
@@ -1,19 +1,87 @@
 package com.group.libraryapp.calculator
 
+import java.lang.Exception
+import java.lang.IllegalArgumentException
+
 fun main() {
     val calculatorTest = CalculatorTest()
     calculatorTest.addTest()
+    calculatorTest.minusTest()
+    calculatorTest.multiplyTest()
+    calculatorTest.divideTest()
+    calculatorTest.divideExceptionTest()
 }
 
 class CalculatorTest {
     fun addTest() {
+        // given
         val calculator = Calculator(5)
+
+        // whend
         calculator.add(3)
 
-        val expectedCalculator = Calculator(8)
-
-        if (calculator != expectedCalculator) {
+        // then
+        if (calculator.number != 8) {
             throw IllegalStateException()
         }
+    }
+
+    fun minusTest() {
+        // given
+        val calculator = Calculator(5)
+
+        // whend
+        calculator.minus(3)
+
+        // then
+        if (calculator.number != 2) {
+            throw IllegalStateException()
+        }
+    }
+
+    fun multiplyTest() {
+        // given
+        val calculator = Calculator(5)
+
+        // whend
+        calculator.multiply(3)
+
+        // then
+        if (calculator.number != 15) {
+            throw IllegalStateException()
+        }
+    }
+
+    fun divideTest() {
+        // given
+        val calculator = Calculator(5)
+
+        // whend
+        calculator.divide(2)
+
+        // then
+        if (calculator.number != 2) {
+            throw IllegalStateException()
+        }
+    }
+
+    fun divideExceptionTest() {
+        //given
+        val calculator = Calculator(5)
+
+        // when
+        try {
+            calculator.divide(0)
+        } catch (e: IllegalArgumentException) {
+            if (e.message != "0으로 나눌 수 없습니다.") {
+                throw IllegalStateException("메시지가 다릅니다.")
+            }
+            // test success!!
+
+            return
+        } catch (e: Exception) {
+            throw IllegalStateException()
+        }
+        throw IllegalStateException("기대한 에러가 발생하지 않았습니다.")
     }
 }
